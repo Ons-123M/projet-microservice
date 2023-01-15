c88c97007e0a4a2a3319b4c8539f93179d66edbd
ons <onsloukil20@gmail.com>
2023-01-15 13:57:16 +0100

    code projet microservice

---
+++ order-service/order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -0,0 +1,60 @@
+package com.programmingtechie.orderservice.service;
+
+import com.programmingtechie.orderservice.dto.InventoryResponse;
+import com.programmingtechie.orderservice.dto.OrderLineItemsDto;
+import com.programmingtechie.orderservice.dto.OrderRequest;
+import com.programmingtechie.orderservice.model.Order;
+import com.programmingtechie.orderservice.model.OrderLineItems;
+import com.programmingtechie.orderservice.repositoriy.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDo)
+                .toList();
+        order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(orderLineItem -> orderLineItem.getSkuCode())
+                .toList();
+
+        // Call Inventory Service and place order if product is in stock
+        InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()//url of the inventory service
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                .retrieve()
+                        .bodyToMono(InventoryResponse[].class)
+                                .block();
+        boolean allProductsUnStock = Arrays.stream(inventoryResponsArray).allMatch(inventoryResponse -> inventoryResponse.isInStock());
+        if(allProductsUnStock){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Produit n'est pas dans le stock, veuillez essayer ultérieurement");
+        }
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDo(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
